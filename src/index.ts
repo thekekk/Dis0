e2342fb41ed506e3f2d088b68ae26705e2c5474b
Shobhit Gupta <sguptamds94@gmail.com>
2022-02-14 18:55:02 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import fs from 'fs';
 import { listen, createServer, getChannels } from './modules/discord';
 
+const keepAlive = require("../server");
+keepAlive();
+
 if (fs.existsSync('./map.json')) {
   listen();
 } else {
