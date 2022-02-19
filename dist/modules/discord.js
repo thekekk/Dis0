a1188a0faf6818dcd57480f087eb160b03403ee6
Shobhit Gupta <sguptamds94@gmail.com>
2022-02-19 17:48:00 +0530

    Update discord.js

--- dist/modules/discord.js
+++ dist/modules/discord.js
@@ -3,12 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createServer = exports.getChannels = exports.listen = exports.createChannel = exports.executeWebhook = exports.createWebhook = void 0;
 const ws_1 = __importDefault(require("ws"));
 const jsonfile_1 = __importDefault(require("jsonfile"));
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const env_1 = require("../util/env");
-const createWebhook = async (channelId) => node_fetch_1.default(`https://discord.com/api/v8/channels/${channelId}/webhooks`, {
+exports.createWebhook = async (channelId) => node_fetch_1.default(`https://discord.com/api/v8/channels/${channelId}/webhooks`, {
     method: 'POST',
     headers: env_1.headers,
     body: JSON.stringify({
@@ -16,8 +15,7 @@ const createWebhook = async (channelId) => node_fetch_1.default(`https://discord
     }),
 }).then((res) => res.json())
     .then((json) => `https://discord.com/api/v8/webhooks/${json.id}/${json.token}`);
-exports.createWebhook = createWebhook;
-const executeWebhook = async ({ content, embeds, username, url, avatar, }) => node_fetch_1.default(url, {
+exports.executeWebhook = async ({ content, embeds, username, url, avatar, }) => node_fetch_1.default(url, {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
@@ -29,8 +27,19 @@ const executeWebhook = async ({ content, embeds, username, url, avatar, }) => no
         avatar_url: avatar,
     }),
 });
-exports.executeWebhook = executeWebhook;
-const createChannel = async (name, pos, newId, parentId) => node_fetch_1.default(`https://discord.com/api/v8/guilds/${newId}/channels`, {
+exports.executeWebhooks = async ({ content, embeds, username, url, avatar, }) => node_fetch_1.default(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        content,
+        embeds,
+        username,
+        avatar_url: avatar,
+    }),
+});
+exports.createChannel = async (name, pos, newId, parentId) => node_fetch_1.default(`https://discord.com/api/v8/guilds/${newId}/channels`, {
     method: 'POST',
     headers: env_1.headers,
     body: JSON.stringify({
@@ -39,8 +48,7 @@ const createChannel = async (name, pos, newId, parentId) => node_fetch_1.default
         position: pos,
     }),
 }).then((res) => res.json());
-exports.createChannel = createChannel;
-const listen = async () => {
+exports.listen = async () => {
     const serverMap = jsonfile_1.default.readFileSync('./map.json');
     const socket = new ws_1.default('wss://gateway.discord.gg/?v=6&encoding=json');
     let authenticated = false;
@@ -87,7 +95,7 @@ const listen = async () => {
                     const hookContent = {
                         content,
                         embeds,
-                        username: `${username}#${discriminator}`,
+                        username: `${username}`,
                         url: webhookUrl,
                         avatar: avatarUrl,
                     };
@@ -99,14 +107,12 @@ const listen = async () => {
         }
     });
 };
-exports.listen = listen;
-const getChannels = async () => node_fetch_1.default(`https://discord.com/api/v8/guilds/${env_1.serverId}/channels`, {
+exports.getChannels = async () => node_fetch_1.default(`https://discord.com/api/v8/guilds/${env_1.serverId}/channels`, {
     method: 'GET',
     headers: env_1.headers,
 }).then((res) => res.json())
     .then((json) => json);
-exports.getChannels = getChannels;
-const createServer = async (channels) => {
+exports.createServer = async (channels) => {
     console.log('Creating mirror server...');
     const cleanedChannels = channels.map(({ id, parent_id, guild_id, last_message_id, ...rest }) => rest);
     const categories = cleanedChannels.filter((channel) => channel.type === 4);
@@ -148,5 +154,4 @@ const createServer = async (channels) => {
         resolve();
     });
 };
-exports.createServer = createServer;
 //# sourceMappingURL=discord.js.map
